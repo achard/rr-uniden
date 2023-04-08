0d46fdb173dfd1cc77b6b80a1616aacecc4e3d0c
OCN\Alex.Chard <alex.chard@gmail.com>
2023-04-09 00:17:25 +1000

    Move test to separate file

--- uniden/base_classes.py
+++ uniden/base_classes.py
@@ -129,36 +129,3 @@ class UnidenTextType:
             raise TypeError(f"Text does not match {cls.__name__} type")
         return cls(text)
 
-
-def test_uniden_bool():
-    x = UnidenBool()
-    assert x.value is False
-    x = UnidenBool('Off')
-    assert x.value is False
-    x = UnidenBool('On')
-    assert x.value is True
-    x = UnidenBool(False)
-    assert str(x) == "Off"
-    assert x.value is False
-    x = UnidenBool(True)
-    assert str(x) == 'On'
-    assert x.value is True
-
-
-def test_uniden_range():
-    x = UnidenRange()
-    assert str(x) == "0.000000\t0.000000\t0.0\tCircle"
-
-
-def test_alert_tone():
-    x = AlertTone()
-    assert x.value == 0
-    assert x.volume == 0
-    assert str(x) == "Off\tAuto"
-
-
-def test_alert_light():
-    x = AlertLight()
-    assert x.state == "On"
-    assert x.colour == "Off"
-    assert str(x) == "Off\tOn"
